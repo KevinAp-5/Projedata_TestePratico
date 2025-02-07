92aa2fceca25ec0950f7362dc88802e589005e29
KevinAp-5 <keven.clash3@gmail.com>
2025-02-06 21:35:09 -0300

    fix: removido comentários da entity Pessoa

--- src/main/entities/Pessoa.java
+++ src/main/entities/Pessoa.java
@@ -2,7 +2,6 @@ package src.main.entities;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 public class Pessoa {
     private String nome;
@@ -13,11 +12,6 @@ public class Pessoa {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
     }
-
-    // public Pessoa(String nome, String dataNasicmento) {
-    // this.nome = nome;
-    // this.dataNascimento = stringToLocalDate(dataNasicmento);
-    // }
 
     public Pessoa() {
     }
@@ -45,38 +39,4 @@ public class Pessoa {
         return dataTimeFormatter.format(this.getDataNascimento());
     }
 
-    // @Override
-    // public int hashCode() {
-    //     final int prime = 31;
-    //     int result = 1;
-    //     result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-    //     return result;
-    // }
-
-    // @Override
-    // public boolean equals(Object obj) {
-    //     if (this == obj)
-    //         return true;
-    //     if (obj == null)
-    //         return false;
-    //     if (getClass() != obj.getClass())
-    //         return false;
-    //     Pessoa other = (Pessoa) obj;
-    //     if (nome == null) {
-    //         if (other.nome != null)
-    //             return false;
-    //     } else if (!nome.equals(other.nome))
-    //         return false;
-    //     return true;
-    // }
-
-    // private LocalDate stringToLocalDate(String dataNascimento) {
-    // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
-    // formatter = formatter.withLocale(putAppropriateLocaleHere); // Locale
-    // specifies human language for translating,
-    // // and cultural norms for lowercase/uppercase and
-    // // abbreviations and such. Example: Locale.US or
-    // // Locale.CANADA_FRENCH
-    // LocalDate date = LocalDate.parse("2005-nov-12", formatter);
-    // }
 }
